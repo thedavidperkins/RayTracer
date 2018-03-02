de6d1e25c56aa094820a8d68f9a30992b60338f9
DJP <thedavidperkins@gmail.com>
2018-03-01 18:24:09 -0800

    added timer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <FreeImage.h>
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -10,6 +11,7 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	for (int i = 1; i < argc; ++i) {
+		std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 		RayTracer rt(10);
 		if (!readFile(argv[i], rt)) {
 			return 1;
@@ -19,8 +21,12 @@ int main(int argc, char *argv[]) {
 		FreeImage_Initialise();
 		FIBITMAP* img = FreeImage_ConvertFromRawBits(rt.getPixels(), rt.getWidth(), rt.getHeight(), rt.getWidth() * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, true);
 		std::string outFile = rt.outFileName == "" ? "out.png" : rt.outFileName;
-		std::cout << "saving to " << outFile << std::endl;
+		std::cout << "saving to " << outFile;
 		FreeImage_Save(FIF_PNG, img, outFile.c_str(), 0);
 		FreeImage_DeInitialise();
+
+		std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+		std::chrono::duration<double> elapsed = end - start;
+		std::cout << " -- Duration: " << elapsed.count() << std::endl;
 	}
 }
